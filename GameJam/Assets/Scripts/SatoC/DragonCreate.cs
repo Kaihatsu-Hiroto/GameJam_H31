8aa7f74690f62c6c5db6a6963933d76b502e6ccf
CSato2000 <bambeaman334@gmail.com>
2019-03-20 12:39:40 +0900

    【竜生産】一定コンボで竜の出現速度と移動スピードが上がるように調整
    竜が死ぬと即座に竜が出荷されるようにしたいです。

--- GameJam/Assets/Scripts/SatoC/DragonCreate.cs
+++ GameJam/Assets/Scripts/SatoC/DragonCreate.cs
@@ -6,39 +6,77 @@ public class DragonCreate : MonoBehaviour
 {
     public GameObject[] dragon;  //竜のオブジェクト配列(5種類)
     public int frameCount;       //フレームカウント
-    public int createCount;      //生成された竜の数
+    public float createSpeed;    //出現間隔
+    public float createCount;    //生成された竜の数
+    public int comboCount;       //コンボ数(外部取得)
+    public float comboMag;       //コンボ倍率
+
+
 
     // Start is called before the first frame update
-    void Start() {
-
+    void Start()
+    {
+        frameCount = 0;
+        createCount = 0;
+        comboCount = FindObjectOfType<Decision>().cnt;
+        comboMag = 0.0f;
+        createSpeed = 250.0f*(1.0f-comboMag);
     }
 
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+
+        comboCount = FindObjectOfType<Decision>().cnt;
         frameCount++;
 
+        createSpeed = SpeedMag();
+
         //一定時間ごとに竜を生成
-        if (frameCount % 250 == 0) {
-            Instantiate(dragon[RandCreate()] , new Vector3(12.0f , 0.0f , 0.0f) , Quaternion.identity);
+        if (frameCount % createSpeed == 0)
+        {
+            Instantiate(dragon[RandCreate()], new Vector3(12.0f, 0.0f, 0.0f), Quaternion.identity);
+            frameCount = 0;
             //生成数の加算
             createCount++;
         }
 
-
-
     }
 
-    int RandCreate() {
+    int RandCreate()
+    {
+
         int num = 0;            //竜の抽選番号
         bool goldflag = false;
 
-        if (createCount == 29 || createCount == 59) goldflag = true;
+        if (createCount>0 && createCount%10==0) goldflag = true;
 
-        num = Random.Range(0 , 4);
+        num = Random.Range(0, 4);
 
         if (goldflag == true) num = 4;
 
         return num;
-    }
+    }//RandCreate終了
+
+
+    float SpeedMag()
+    {
+        //5コンボずつ出現速度が上がる
+        if (comboCount > 0 && comboCount >= 5)
+        {
+            return 250.0f * 0.35f;
+
+        }
+        else if (comboCount > 0 && comboCount >= 10)
+        {
+
+            return 250.0f * 0.25f;
+
+        }
+        else
+        {
+            return 250.0f * 0.8f;
+        }
+    }//SpeedMag終了
 }
